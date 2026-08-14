5a80508eb048ddcc00212eaf054aecea6d19eaec
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-13 22:12:08 -0500

    .cs 08-13-2026 0344-reverse-string

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -1,10 +1,9 @@
 public class Solution {
     public void ReverseString(char[] s) {
-        char temp;
         int right = s.Length-1;
         for ( int i =0; i< right ;i++)
         {
-            temp = s[i];
+            char temp = s[i];
             s[i] = s[right];
             s[right] = temp;
             right --;
