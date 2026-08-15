74714b9cadec8f18c81c9e461184e0a978dcee59
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-15 00:24:36 -0500

    .cs 08-15-2026 0500-keyboard-row

--- 0500-keyboard-row/0500-keyboard-row.cs
+++ 0500-keyboard-row/0500-keyboard-row.cs
@@ -2,7 +2,6 @@ public class Solution {
     public string[] FindWords(string[] words) {
       List<string> lisOutput = new List<string>();
       string[] rows = ["qwertyuiopQWERTYUIOP","asdfghjklASDFGHJKL","zxcvbnmZXCVBNM"];
-      //then see the row required from the first letter
       foreach(string word in words)
       {
         int rowNum = Array.FindIndex(rows, row => row.Contains(word[0]));
@@ -20,7 +19,6 @@ public class Solution {
             
         }
       }
-      return lisOutput.ToArray();
-      //go through the word to see if everything on same row (if so add to soultions)  
+      return lisOutput.ToArray();  
     }
 }
