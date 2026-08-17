ec7833e3723f8db8c8b436a19e8a7e14321f20a3
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-17 12:22:09 -0500

    .cs 08-17-2026 0021-merge-two-sorted-lists

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+    ListNode nodAnchor = new ListNode(0);
+    ListNode nodBuild = nodAnchor;
+
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val <= list2.val)
+            {
+                nodBuild.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+            nodBuild.next = list2;
+                list2 = list2.next; 
+            }
+            nodBuild = nodBuild.next;
+        }
+        if (list1 != null)
+        {
+            nodBuild.next = list1;
+        }
+        else
+        {
+            nodBuild.next = list2;
+        }
+        return nodAnchor.next;
+    }
+}
