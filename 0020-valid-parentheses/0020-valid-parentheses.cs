2a66d7830f031285121b382f6e6f0d3795cd7ef4
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-17 11:54:56 -0500

    .cs 08-17-2026 0020-valid-parentheses

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsValid(string s) {
+        while(s.Contains("()") ||s.Contains("[]") || s.Contains("{}"))
+        {
+            s = s.Replace("()", "");
+            s = s.Replace("[]", "");
+            s = s.Replace("{}", "");
+        }
+        if(s.Length ==0)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }
+}
