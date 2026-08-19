7e0d6617341e6ac68611199c68ecf323c21e594e
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-19 15:39:52 -0500

    .py 08-19-2026 0058-length-of-last-word

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,5 +1,9 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         sSplit = s.split()
         return len(sSplit[-1])
         
