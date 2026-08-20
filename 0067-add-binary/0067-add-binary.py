210cf37822dfefacaf51cc868c0d903319087fa7
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-20 16:03:54 -0500

    .py 08-20-2026 0067-add-binary

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        Output =""
+        i = len(a)-1
+        j = len(b)-1
+        carry =0
+        while i>=0 or j>=0 or carry>0:
+            if i>=0 :
+                if a[i] == '1':
+                    carry += 1
+                i -=1
+            if j>=0 :
+                if b[j] == '1':
+                    carry += 1
+                j -=1
+            Output = str(carry%2) + Output
+            carry //=2
+        return Output
