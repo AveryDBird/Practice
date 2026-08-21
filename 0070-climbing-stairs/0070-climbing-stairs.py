f4a7b52b9948be0fbeb22367acc8f0cd6a0bb4b5
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-21 18:36:33 -0500

    .py 08-21-2026 0070-climbing-stairs

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,13 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         if n<= 2:
             return n
         listSteps = [0,1,2]
         for i in range (3, n+1):
             listSteps.append(listSteps[i-1] + listSteps[i-2])
         return listSteps[n]
-        
-
         
