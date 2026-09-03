31674a1cd8ced60992093cff143b5863b28076d8
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-09-03 17:55:10 -0500

    .py 09-03-2026 0083-remove-duplicates-from-sorted-list

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -1,10 +1,14 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
         current = head
         while current is not None and current.next is not None:
             if current.val == current.next.val:
@@ -12,4 +16,3 @@ class Solution:
             else:
                 current = current.next
         return head
-        
